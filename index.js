ea44677d94d2cd218d565b10956cb23044c26131
effiecancode <faitheffie25@gmail.com>
2023-12-02 13:38:33 +0300

    all tests passed 🤩

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // hello js functions parameter lab
+
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+}
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+const info = introductionWithLanguage('effie', 'js')
+console.log(info);
+
+function introductionWithLanguageOptional(name, language='JavaScript') {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+const info2 = introductionWithLanguageOptional('effie')
+console.log(info2);
